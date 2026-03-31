3061bbf4cfafc77310f8e80af602c09e0dc7f3bf
gennarikun <gennarikun@gmail.com>
2026-04-01 06:59:43 +0900

    Initialize clean static site

---
+++ footer.php
@@ -0,0 +1,76 @@
+<footer class="section siteFooter">
+		<div class="footerMenu">
+			<div class="container">
+				<nav class="menu-menu-container">
+					<ul id="menu-menu-1" class="menu nav">
+						<li id="menu-item-2029"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2029"><a
+								href="/contact/">お申込み・アクセス</a></li>
+						<li id="menu-item-2473"
+							class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-2473">
+							<a href="/menu/">メニュー・料金</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-page"><a
+								href="/dantai/">団体ランチ</a></li>
+						<li id="menu-item-2024"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2024"><a
+								href="/voice/">お客様の声・感想</a></li>
+						<li id="menu-item-2025"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2025"><a
+								href="/gallery/">体験ギャラリーページ</a></li>
+						<li id="menu-item-2023"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2023"><a
+								href="/sponsor/">富士家スポンサー様</a></li>
+						<li id="menu-item-2610"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2610"><a
+								href="/about-suridane/">お土産すりだね紹介</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+
+
+		<div class="container sectionBox copySection text-center">
+			<p>Copyright © 山中湖の体験教室【富士家】｜ ほうとう・そば・吉田のうどん｜友達同士・家族も大歓迎 All Rights Reserved.</p>
+			<p>Powered by <a href="https://wordpress.org/">WordPress</a> &amp; <a href="https://lightning.nagoya/ja/"
+					target="_blank" title="Free WordPress Theme Lightning"> Lightning Theme</a> by Vektor,Inc.
+				technology.</p>
+		</div>
+	</footer>
+<div id="vk-mobile-nav-menu-btn" class="vk-mobile-nav-menu-btn">MENU</div>
+	<div class="vk-mobile-nav vk-mobile-nav-drop-in" id="vk-mobile-nav">
+		<nav class="vk-mobile-nav-menu-outer" role="navigation">
+			<ul id="menu-menu-2" class="vk-menu-acc menu">
+				<li
+					class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2029">
+					<a href="/contact/">お申込み・アクセス</a>
+					<ul class="sub-menu">
+						<li id="menu-item-2028"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2028"><a
+								href="/rule/">注意事項</a></li>
+					</ul>
+				</li>
+				<li
+					class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2473">
+					<a href="/menu/">メニュー・料金</a>
+					<ul class="sub-menu">
+						<li id="menu-item-2837"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2837"><a
+								href="/omiyage/">お食事なし麺作り体験メニュー解説</a></li>
+						<li id="menu-item-3565"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3565">
+							<a href="/" aria-current="page">山梨県プレミアム体験プラン</a></li>
+					</ul>
+				</li>
+				<li class="menu-item menu-item-type-post_type menu-item-object-page"><a
+						href="/dantai/">団体ランチ</a></li>
+				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2024"><a
+						href="/voice/">お客様の声・感想</a></li>
+				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2025"><a
+						href="/gallery/">体験ギャラリーページ</a></li>
+				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2023"><a
+						href="/sponsor/">富士家スポンサー様</a></li>
+				<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2610"><a
+						href="/about-suridane/">お土産すりだね紹介</a></li>
+			</ul>
+		</nav>
+	</div>
---
+++ header.php
@@ -0,0 +1,49 @@
+<header class="siteHeader">
+		<div class="container siteHeadContainer">
+			<div class="navbar-header">
+				<p class="navbar-brand siteHeader_logo">
+					<a href="/">
+						<span><img src="/assets/389476.jpg" alt="山中湖の体験教室【富士家】｜ ほうとう・そば・吉田のうどん｜友達同士・家族も大歓迎"></span>
+					</a>
+				</p>
+			</div>
+
+			<div id="gMenu_outer" class="gMenu_outer">
+				<nav class="menu-menu-container">
+					<ul id="menu-menu" class="menu gMenu vk-menu-acc">
+						<li id="menu-item-2029"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children"><a
+								href="/contact/"><strong class="gMenu_name">お申込み・アクセス</strong></a>
+							<ul class="sub-menu">
+								<li id="menu-item-2028"
+									class="menu-item menu-item-type-post_type menu-item-object-page"><a
+										href="/rule/">注意事項</a></li>
+							</ul>
+						</li>
+						<li id="menu-item-2473"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children">
+							<a href="/menu/"><strong class="gMenu_name">メニュー・料金</strong></a>
+							<ul class="sub-menu">
+								<li id="menu-item-2837"
+									class="menu-item menu-item-type-post_type menu-item-object-page"><a
+										href="/omiyage/">お食事なし麺作り体験メニュー解説</a></li>
+								<li id="menu-item-3565"
+									class="menu-item menu-item-type-post_type menu-item-object-page">
+									<a href="/">山梨県プレミアム体験プラン</a></li>
+							</ul>
+						</li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-page"><a
+								href="/dantai/"><strong class="gMenu_name">団体ランチ</strong></a></li>
+						<li id="menu-item-2024" class="menu-item menu-item-type-post_type menu-item-object-page"><a
+								href="/voice/"><strong class="gMenu_name">お客様の声・感想</strong></a></li>
+						<li id="menu-item-2025" class="menu-item menu-item-type-post_type menu-item-object-page"><a
+								href="/gallery/"><strong class="gMenu_name">体験ギャラリーページ</strong></a></li>
+						<li id="menu-item-2023" class="menu-item menu-item-type-post_type menu-item-object-page"><a
+								href="/sponsor/"><strong class="gMenu_name">富士家スポンサー様</strong></a></li>
+						<li id="menu-item-2610" class="menu-item menu-item-type-post_type menu-item-object-page"><a
+								href="/about-suridane/"><strong class="gMenu_name">お土産すりだね紹介</strong></a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
